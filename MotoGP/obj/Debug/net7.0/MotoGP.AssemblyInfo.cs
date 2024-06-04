019f6d096c1a8c455ba8280d2c82eda2f7de5828
Nathan Neve <nathanneve@live.be>
2024-06-04 18:13:23 +0200

    desktop study sesh

--- MotoGP/obj/Debug/net7.0/MotoGP.AssemblyInfo.cs
+++ MotoGP/obj/Debug/net7.0/MotoGP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MotoGP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5174023d20c256a2cbadeece2471a27aa7295f3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MotoGP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MotoGP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ examenprep/www/MusicStore/MusicStore/Controllers/ShoppingCartController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MusicStore.Data;
+using MusicStore.Models;
+using MusicStore.Models.ViewModels;
+using System.Linq;
+using System.Net.Http;
+
+namespace MusicStore.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        private readonly StoreContext _context;
+
+        public ShoppingCartController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var shoppingCart = new ShoppingCart(HttpContext, _context);
+            var cartItems = shoppingCart.GetCartItems();
+
+            ListShoppingCartViewModel shoppingCartVM = new ListShoppingCartViewModel();
+            shoppingCartVM.CartItems = cartItems;
+            shoppingCartVM.cartTotal = (int)HttpContext.Session.GetInt32("CartTotal");
+            return View(shoppingCartVM);
+        }
+
+        public IActionResult AddToCart(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var album = _context.Albums.FirstOrDefault(a => a.AlbumID == id);
+            if (album == null)
+            {
+                return NotFound();
+            }
+
+            var shoppingCart = new ShoppingCart(HttpContext, _context);
+            int existingCartTotal = HttpContext.Session.GetInt32("CartTotal") ?? 0;
+            shoppingCart.AddCartTotal(existingCartTotal + album.Price);
+            shoppingCart.AddToCart(album);
+            HttpContext.Session.SetInt32("CartTotal", shoppingCart.CartTotal);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult RemoveFromCart(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var shoppingCart = new ShoppingCart(HttpContext, _context);
+            shoppingCart.RemoveFromCart((int)id);
+            return RedirectToAction("Index");
+        }
+    }
+}
